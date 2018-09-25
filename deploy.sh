ad4a0b6991413b1031042252d10a9ff36a5b3266
theniceangel <theniceangel@163.com>
2018-09-25 12:25:55 +0800

    modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:theniceangel/learning-vuex.git master:gh-pages
+git push -f git@github.com:theniceangel/learn-vuex.git master:gh-pages
 
 cd -
